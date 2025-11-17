aa841962987e98209782b316c04d8495326ffe0f
Zainab Kinza Sheikh <zainabkinza.s@yahoo.com>
2025-11-18 00:57:47 +0500

    Add files via upload

---
+++ Number of 1 bits (Leetcode).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int count = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        if (n % 2 == 1) {  
+            count++;
+        }
+        n = n / 2;  
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
