b4f79b0b04bd650667133864af380af30af1e683
Zainab Kinza Sheikh <zainabkinza.s@yahoo.com>
2025-11-18 02:12:43 +0500

    Add files via upload

---
+++ Power of four (Leetcode).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter an integer n: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("false");
+        return 0;
+    }
+
+    while (n % 4 == 0) {
+        n = n / 4;
+    }
+
+    if (n == 1) {
+        printf("true");
+    } else {
+        printf("false");
+    }
+
+    return 0;
+}
